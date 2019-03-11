69f155df96723cd18fcc66e622c778c93ba94919
perzonas <andre.perzon@hotmail.com>
2019-03-11 13:21:17 +0100

    added to backend

--- Mininet/backend/backend.py
+++ Mininet/backend/backend.py
@@ -7,13 +7,17 @@ import json
 
 class Server:
     test = "127.0.0.1"
+    ip = "20.1.90."
     port = 1337
     ownIP = "20.1.90."
+    hostID = 0
     numberofhost = 0
+    logicalclock = 0
 
     def run(self, hostnumber, numberofhosts=1):
         self.numberofhost = numberofhosts
         self.ownIP += str(hostnumber)
+        self.hostID = hostnumber
 
         # AF_INET -> ipv4 and SOCK_STREAM -> tcp
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,13 +44,57 @@ class Server:
                 print("Exception when accepting connection or creating thread.")
                 print(exception)
 
+    # handling incoming connection
     def handleconnection(self, connection, connectinfo):
-        print("handling connection")
-        host, port = connectinfo
-        self.sendmessage(("hejsan", 12, 90), host, port, connection)
+        print("handling connection from ", connectinfo)
+        ip, port = connectinfo
+        id = ip.split(".")[-1]
+        message, type = self.retrievemessage(connection)
+        print(message)
+        print(type)
+
+        #host, port = connectinfo
+        #self.sendmessage(("hejsan", 12, 90), host, port, connection)
+
+    def retrievemessage(self, connection):
+        id = 0
+        data = ""
+        type = 0
+
+
+        while True:
+            byte = connection.recv(1)
+            byte = byte.decode()
+            if byte == ";":
+                break
+            elif byte:
+                data += byte
+            else:
+                break
+        type = int(data)
+        data = ""
+        while True:
+            byte = connection.recv(1)
+            byte = byte.decode()
+            if byte == ";":
+                connection.close()
+                break
+            elif byte:
+                data += byte
+            else:
+                break
+        message = json.loads(data)
+        return message, type
 
     def broadcast(self, message, ):
-        print("Broadcasting message")
+        print("Broadcasting message to all hosts")
+        for host in range(1, (self.numberofhost+1)):
+            host = self.ip + host
+
+            # do not send to ourselves
+            if host != self.ownIP:
+                self.sendmessage(message, host, self.port)
+
 
     # sending message to another host
     def sendmessage(self, message, host, port, connection):
@@ -60,6 +108,8 @@ class Server:
         print(host, port)
         # sock.connect((host, port))
         print(serializeddata)
+        connection.send((str(len(serializeddata))+";").encode(), (len(serializeddata)+1))
+        print(len(serializeddata))
 
         connection.sendall((serializeddata+";").encode())
 
--- Mininet/test.py
+++ Mininet/test.py
@@ -1,25 +1,33 @@
 import socket
+import json
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 1337        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    while True:
+    print("sending hello world")
+    s.send((str(120)+";").encode(), (len(str(120))+1))
+    s.sendall((json.dumps("Hello world")+";").encode())
+    if True:
         data = []
         byte = s.recv(1)
-        byte = byte.decode()
-        print(byte)
-        if byte == ";":
-            s.close()
-            print(data)
-            break
-        elif byte:
-            print("ih")
-            data.append(byte)
-        else:
-            break
+        message = ""
+        while True:
+            if byte.decode() == ";":
+                s.close()
+                print(data)
+                break
+            elif byte:
+                message += byte.decode()
+                byte = s.recv(1)
+            else:
+                break
+        print(message)
+
+        message = message[2:]
+        converted = json.loads(message)
+        print(converted[0])
 
     """ while True:
         data = s.recv(1)
